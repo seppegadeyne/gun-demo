6325ebe8dcc2de27b7c9b3cfa7766a0a6d6dae7f
Seppe Gadeyne <seppe@fushia.be>
2021-10-15 15:17:33 +0200

    Update

--- src/lib/Gun.svelte
+++ src/lib/Gun.svelte
@@ -1,47 +1,47 @@
 <script>
     import { onMount } from 'svelte'
+    import Gun from 'gun'
 
-    let hello
-
-
-    // Initialize gun.
-    // [localStorage] We are turning localStorage to false for testing purposes. Generally, you'd want that to be true
-    // [secret] Secret should be something long and secure. Your data will be saved to Skynet using that secret
-    // [portal] Skynet portal you'd like to use. Use a portal you trust or run your own. They could potentially manipulate your data (although I don't see why)
-    // [debug] Show debug output
-    // [until] Change the batch time of Gun so that it doesn't attempt to write to storage too quickly
-    onMount(() => {
-        window.gun = new Gun({
-            localStorage: false,
-            secret: "TopGeheimSecretWord",
-            portal: "https://siasky.net",
-            debug: false,
-            until: 2*1000
-        })
-
-        // Put data into gun. This will store in memory, peers, then localStorage (disabled), then Skynet
-        /* gun.get('hello').put(
-            {
-                name: "Seppe"
-            }
-        ); */
-
-        // Get data into gun. This will pull from memory, peers, then localStorage (disabled), then Skynet
-        gun.get('hello').on(data => {
-            hello = data['name']
-            console.log("hello: " + hello)
-        })
+    const gun = Gun({
+        peers: [
+            'https://gun.straffesites.com/gun'
+        ]
     })
 
+    //const gun = Gun()
+
+    let
+        name = 'Hello',
+        lastname = 'World',
+        text
+
+    function save(name, lastname) {
+        gun.get('store').put(
+            {
+                name: name,
+                lastname: lastname
+            }
+        )
+    }
+
+    gun.get('store').on(data => {
+        console.log("data changed or loaded: " + JSON.stringify(data))
+        text = JSON.stringify(data)
+        name = data.name
+        lastname = data.lastname
+    })
+
+    $: save(name, lastname)
 </script>
 
-<svelte:head>
-    <script src="https://cdn.jsdelivr.net/npm/gun/gun.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/gun/lib/radix.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/gun/lib/radisk.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/gun/lib/store.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/gun/lib/rindexed.js"></script>
-    <script src="https://unpkg.com/zenbase/dist/main.js"></script>
-</svelte:head>
+<h1>GUN example</h1>
+Name: <input bind:value={name} type="text">
+Lastname: <input bind:value={lastname} type="text" style="margin-bottom:2rem;">
 
-GUN LOADED
+<div style="padding:2rem; background-color:lightgreen; margin-bottom: 2rem;">
+    {text}
+</div>
+
+<div style="padding:2rem; background-color:lightblue;">
+    Have a look at the console; when database gets updated it pushes data to the client.
+</div>
